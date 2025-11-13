316b3ad2b5d15ff0bf97ed47f990b0c11a39676f
ANKUSH <ankushchoudhary2019@gmail.com>
2025-11-13 00:31:55 -0800

    ✅ Added new Vercel frontend to CORS whitelist

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,11 @@ const PORT = process.env.PORT || 5000;
 //  Allowed frontend origins
 const FRONTEND_URLS = [
   process.env.FRONTEND_URL,
-  'http://localhost:3003',
   'https://portfolio-seven-kappa-78.vercel.app',
   'https://portfolio-git-main-mrankush079s-projects.vercel.app',
   'https://portfolio-q375val6j-mrankush079s-projects.vercel.app',
-  'https://portfolio-uht3.onrender.com',
-  'https://portfolio-mxq8cr210-mrankush079s-projects.vercel.app'
+  'https://portfolio-mxq8cr210-mrankush079s-projects.vercel.app',
+  'http://localhost:3003'
 ].filter(Boolean);
 
 const corsOptions = {
